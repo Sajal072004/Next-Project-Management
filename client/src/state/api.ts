18fedceb66f1edcff6b8039b95acd92e2cfa1e4d
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-02 07:34:49 +0530

    [feat]:setup redux toolkit

---
+++ client/src/state/api.ts
@@ -0,0 +1,5 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL})
+})
