237c208361a53842db44cb37632634ef4ff866a7
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-02 16:58:24 +0530

    [feat]:setup routes for project

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express' 
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+import projectRoutes from './routes/project-routes'
+
+// Route Imports
+
+// Configurations
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+app.get("/" , (req ,res) => {
+  res.send("this is home route");
+});
+
+app.use("/projects" , projectRoutes);
+
+// server
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT , () => {
+  console.log(`server running at port ${PORT}`);
+})
