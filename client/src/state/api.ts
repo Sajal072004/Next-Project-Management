ffb163d3701c609596a980749d6599840ad1b76c
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-04 21:02:08 +0530

    [feat]: implement priorities page

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -108,6 +108,14 @@ export const api = createApi({
         result ? result.map(({ id }) => ({ type: "Tasks" as const, id })) : [{ type: "Tasks" as const }],
     }),
 
+    getTasksByUser: build.query<Task[], number>({
+      query: (userId) => `tasks/user/${userId}`,
+      providesTags: (result, error, userId) =>
+        result
+          ? result.map(({ id }) => ({ type: "Tasks", id }))
+          : [{ type: "Tasks", id: userId }],
+    }),
+
     createTask: build.mutation<Task, Partial<Task>>({
       query: (task) => ({
         url: "tasks",
@@ -151,5 +159,6 @@ export const {
   useUpdateTaskStatusMutation,
   useSearchQuery,
   useGetUsersQuery,
-  useGetTeamsQuery
+  useGetTeamsQuery,
+  useGetTasksByUserQuery
 } = api;
--- server/dist/src/routes/task-routes.js
+++ server/dist/src/routes/task-routes.js
@@ -6,4 +6,5 @@ const router = (0, express_1.Router)();
 router.get("/", task_controller_1.getTasks);
 router.post('/', task_controller_1.createTask);
 router.patch("/:taskId/status", task_controller_1.updateTaskStatus);
+router.get("/user/:userId", task_controller_1.getUserTasks);
 exports.default = router;
